bfe76cfc112a1ac20716d2782e9e3d48a3c45372
TormiAbel <tormi.abel@tptlive.ee>
2023-02-02 14:12:47 +0200

    Actually fixed AccountModal

--- src/Headers/GlobalHeader.js
+++ src/Headers/GlobalHeader.js
@@ -1,33 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
-import  Constants  from "expo-constants";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Modal,
+  Dimensions,
+  PanResponder,
+} from "react-native";
+import Constants from "expo-constants";
+import AccountModal from "../modals/accountModal";
 
 export default function GlobalHeader({ navigation, AppState }) {
-    return (
-        <View style={styles.headerCont}>
-            <View style={styles.iconCont}>
-                <TouchableOpacity>
-                    <Image
-                        style={styles.logo}
-                        source={require('../assets/images/profile.png')}    
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Image
-                        style={styles.logo}
-                        source={require('../assets/images/search.png')}      
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Image
-                        style={styles.logo}
-                        source={require('../assets/images/notifications.png')}
-                        
-                    />
-                </TouchableOpacity>    
-            </View>
-            
-            {/* <View style={styles.headerText}>
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View style={styles.headerCont}>
+      <View style={styles.iconCont}>
+        <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+          <Image
+            style={styles.logo}
+            source={require("../assets/images/profile.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            style={styles.logo}
+            source={require("../assets/images/search.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            style={styles.logo}
+            source={require("../assets/images/notifications.png")}
+          />
+        </TouchableOpacity>
+      </View>
+      <AccountModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      />
+
+      {/* <View style={styles.headerText}>
                 <Text style={styles.text}>Offers</Text>
                 <Text style={{
                     fontFamily: 'Mulish_500Medium',
@@ -35,34 +49,31 @@ export default function GlobalHeader({ navigation, AppState }) {
                     color: '#7C7C7C'
                 }}>Search for tutoring on specific subjects</Text>
             </View> */}
-            
-        </View>
-    )
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    headerCont: {
-        flex: 1,
-        flexDirection: 'row',
-        width: '100%',
-        marginBottom:0,
-        justifyContent: "space-between",
-        paddingLeft:'7%',
-        paddingRight:'7%',
-        alignContent: "center",
-        paddingTop: Constants.statusBarHeight
-    },
-    logo:{
-        height:27,
-        width: 30
-    },
-   iconCont: {
-   
-    padding:24,
-    
-    flexDirection: 'row',
+  headerCont: {
+    flex: 1,
+    flexDirection: "row",
+    width: "100%",
+    marginBottom: 0,
+    justifyContent: "space-between",
+    paddingLeft: "7%",
+    paddingRight: "7%",
+    alignContent: "center",
+    paddingTop: Constants.statusBarHeight,
+  },
+  logo: {
+    height: 27,
+    width: 30,
+  },
+  iconCont: {
+    padding: 24,
+
+    flexDirection: "row",
     justifyContent: "space-between",
     alignContent: "center",
-    width: '100%',
-    
-   },
-})
+    width: "100%",
+  },
+});
